8e20a90a2061171b7db5f57608c8b7842f0730a3
PRADIP-1596 <9187pradipyadav@gmail.com>
2020-07-07 22:05:02 +0530

    Bet Rsult of the Gambling simulator

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,11 +1,32 @@
-#!/bin/bash-x
-
+#!/bin/bash -x
 
 echo "Welcome To Gambling Simulator"
-
-
+BET_LOSE=0;
+BET_WIN=1;
 DailyAmount=100;
+BetAmount=1;
 
-BetAmmount=1;
+function BetResult()
+{
+
+
+case $1 in
+$BET_WIN)
+			DailyAmount=$((DailyAmount+BetAmount));
+		;;
+		$BET_LOSE)
+			DailyAmount=$((DailyAmount-BetAmount));
+		;;
+		*)
+			echo"It is some error got"
+		;;
+	esac
+	echo $DailyAmount
+}
+
+		BetChance=$((RANDOM%2));
+DailyAmount=$(BetResult $betChance); 
+
+
 
 
