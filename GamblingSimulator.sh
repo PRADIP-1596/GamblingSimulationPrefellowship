5d33441f145be3c1a08d0cae7c976931ea492979
PRADIP-1596 <9187pradipyadav@gmail.com>
2020-07-07 22:29:21 +0530

    TO calculate the luckiest and unluckiest day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,54 +1,73 @@
-#!/bin/bash-x
-
-echo "Welcome To Gambling Simulator"
-BET_LOSE=0;
-BET_WIN=1;
-
-function betResult()
-{
-	case $1 in
-		$BET_WIN)
-			dailyAmount=$((dailyAmount+betAmount));
-		;;
-		$BET_LOSE)
-			dailyAmount=$((dailyAmount-betAmount));
-		;;
-		*)
-			echo"SOME ERROR"
-		;;
-	esac
-	echo $dailyAmount
-}
-amountWon=0;
-amountLost=0;
-netResult=0;
-for ((dayCount=1; dayCount<=20; dayCount++ ))
+#!/bin/bash -x
+printf "Calculate luckiest and unluckiest day of gambler\n"
+#CONSTANT
+BET=1
+WIN=1
+LOSE=0
+#VARIABLES
+luckyDay=0
+unluckyDay=0
+winDayEnd=0
+lostDayEnd=0
+won=0
+lost=0
+days=20
+count=0
+maxWin=0
+maxLose=0
+#GAMBLE FOR 20 DAYS
+while(( $days>0))
 do
-	dailyAmount=100;
-	betAmount=1;
-
-	while ((dailyAmount<=150 && dailyAmount>=50 ))
+	stake=100
+	tempStake=$stake
+	stop=$((stake/2))
+	goal=$((stake+stop))
+	#GAMBLE PER DAY
+	while (( $stake>$stop && $stake<$goal))
 	do
-		betChance=$((RANDOM%2));
-		dailyAmount=$(betResult $betChance);
+		gamble=$((RANDOM%2))
+		if(($gamble==$WIN))
+		then
+			won=$((won+1))
+			stake=$((stake+BET))
+		else
+			lost=$((lost+1))
+			stake=$((stake-BET))
+		fi
 	done
-	if((dailyAmount<100))
+	#CALCULATING WINS AND LOSTS PER DAY AND STORE IT IN ARRAY
+	if(($stake>$tempStake))
 	then
-		amountLost=$((amountLost+50));
+		winDayEnd=50
+		Array[$count]=$winDayEnd
 	else
-		amountWon=$((amountWon+50));
+		lostDayEnd=-50
+		Array[$count]=$lostDayEnd
 	fi
+	count=$((count+1))
+	days=$((days-1))
 done
-echo "Amount Won = $amountWon"
-echo "Amount Lost = $amountLost"
-netResult=$((amountWon-amountLost));
-if ((netResult>=0))
-then
-	echo "Direct Winnings = $DirectResult"
-else
-	netResult=$((netResult* -1 ));
-	echo "Direct Loss = $DirectResult"
-fi
+sum=0
+#CALCULATING LUCKIEST AND UNLUCKIEST DAY
+for((day=0; day<20; day++))
+do
+	sum=$(($sum+"${Array[$day]}"))
+	#CHECK THE MAXIMUM CONTINUOUS WIN AND CONSIDER IT AS LUCKIEST DAY
+	if(($maxWin<=$sum))
+	then
+		maxWin=$sum
+		luckyDay=$((day+1))
+	fi
+	#CHECK THE MAXIMUM CONTINUOUS LOST AND CONSIDER IT AS UNLUCKIEST DAY
+	if(($maxLose>=$sum))
+   then
+      maxLose=$sum
+      unluckyDay=$((day+1))
+   fi
+done
+printf "Luckiest day: $luckyDay, Wins: $maxWin\n"
+printf "unluckiest day: $unluckyDay, Wins: $maxLose\n"
+
 
 
 
