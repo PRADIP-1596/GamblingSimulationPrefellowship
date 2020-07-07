b408e726c3a85150437110826bebb86c63cb8e47
PRADIP-1596 <9187pradipyadav@gmail.com>
2020-07-07 22:09:55 +0530

    Condition Resign Gambling Simulator problem

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,29 +3,29 @@
 echo "Welcome To Gambling Simulator"
 BET_LOSE=0;
 BET_WIN=1;
-DailyAmount=100;
+DailyAmount=200;
 BetAmount=1;
 
 function BetResult()
 {
-
-
-case $1 in
-$BET_WIN)
-			DailyAmount=$((DailyAmount+BetAmount));
+	case $1 in
+		$BET_WIN)
+			dailyAmount=$((dailyAmount+betAmount));
 		;;
 		$BET_LOSE)
-			DailyAmount=$((DailyAmount-BetAmount));
+			dailyAmount=$((dailyAmount-betAmount));
 		;;
 		*)
-			echo"It is some error got"
+			echo"SOME ERROR"
 		;;
 	esac
 	echo $DailyAmount
 }
-
-		BetChance=$((RANDOM%2));
-DailyAmount=$(BetResult $betChance); 
+while ((DailyAmount<=200 && DailyAmount>=50 ))
+do
+	betChance=$((RANDOM%2));
+	DailyAmount=$(betResult $betChance);
+done 
 
 
 
